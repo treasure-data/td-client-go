9673745206ac7104bb6d8cba113989c141250008
kterada0509 <kterada.0509sg@gmail.com>
2017-05-14 18:17:28 +0900

    Add database method test

---
+++ api_database_test.go
@@ -0,0 +1,88 @@
+package td_client
+
+import "testing"
+
+func TestListDatabases(t *testing.T) {
+	client, err := NewTDClient(Settings{
+		Transport: &DummyTransport{[]byte(listDatabasesResponse)},
+	})
+	if err != nil {
+		t.Fatalf("failed create client: %s", err.Error())
+	}
+	databaseList, err := client.ListDatabases()
+	if err != nil {
+		t.Fatalf("bad request: %s", err.Error())
+	}
+	if len(*databaseList) != 3 {
+		t.Fatal("not expected database count")
+	}
+}
+
+func TestDeleteDatabase(t *testing.T) {
+	client, err := NewTDClient(Settings{
+		Transport: &DummyTransport{[]byte(deleteDatabasesResponse)},
+	})
+	if err != nil {
+		t.Fatalf("failed create client: %s", err.Error())
+	}
+	err = client.DeleteDatabase("test")
+	if err != nil {
+		t.Fatalf("bad request: %s", err.Error())
+	}
+}
+
+func TestCreateDatabase(t *testing.T) {
+	client, err := NewTDClient(Settings{
+		Transport: &DummyTransport{[]byte(createDatabasesResponse)},
+	})
+	if err != nil {
+		t.Fatalf("failed create client: %s", err.Error())
+	}
+	err = client.CreateDatabase("test", nil)
+	if err != nil {
+		t.Fatalf("bad request: %s", err.Error())
+	}
+}
+
+const listDatabasesResponse = `
+{
+   "databases":[
+      {
+         "name":"test",
+         "created_at":"2016-11-07 09:37:37 UTC",
+         "updated_at":"2016-11-07 09:37:37 UTC",
+         "count":69664,
+         "organization":null,
+         "permission":"administrator"
+      },
+      {
+         "name":"sample_datasets",
+         "created_at":"2014-10-04 01:13:11 UTC",
+         "updated_at":"2016-04-21 06:31:45 UTC",
+         "count":8812278,
+         "organization":null,
+         "permission":"query_only"
+      },
+      {
+         "name":"information_schema",
+         "created_at":"2016-12-15 05:24:21 UTC",
+         "updated_at":"2016-12-15 05:24:21 UTC",
+         "count":0,
+         "organization":null,
+         "permission":"query_only"
+      }
+   ]
+}
+`
+
+const deleteDatabasesResponse = `
+{
+   "database":"test"
+}
+`
+
+const createDatabasesResponse = `
+{
+   "database":"test"
+}
+`
