554360d7bd0b2d16dde0305120836c64b08888a2
kterada0509 <kterada.0509sg@gmail.com>
2017-09-10 00:26:56 +0900

    Add User List API

---
+++ api_user.go
@@ -0,0 +1,96 @@
+//
+// Treasure Data API client for Go
+//
+// Copyright (C) 2014 Treasure Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package td_client
+
+import "time"
+
+// ListUsersResultElement represents an item of the result of ListUsers API
+type ListUsersResultElement struct {
+	ID            int
+	FirstName     string
+	LastName      string
+	Email         string
+	Phone         string
+	GravatarURL   string
+	Administrator bool
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Name          string
+	AccountOwner  bool
+	Organization  string
+	Roles         []string
+}
+
+// ListUsersResult is a collection of ListUsersResultElement
+type ListUsersResult []ListUsersResultElement
+
+var listUsersSchema = map[string]interface{}{
+	"users": []map[string]interface{}{
+		map[string]interface{}{
+			"id":            0,
+			"first_name":    Optional{"", ""},
+			"last_name":     Optional{"", ""},
+			"email":         "",
+			"phone":         Optional{"", ""},
+			"gravatar_url":  "",
+			"administrator": false,
+			"created_at":    time.Time{},
+			"updated_at":    time.Time{},
+			"name":          "",
+			"account_owner": false,
+			"organization":  Optional{"", ""},
+			"roles":         []string{},
+		},
+	},
+}
+
+func (client *TDClient) ListUsers() (*ListUsersResult, error) {
+	resp, err := client.get("/v3/user/list", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, client.buildError(resp, -1, "List users failed", nil)
+	}
+	js, err := client.checkedJson(resp, listUsersSchema)
+	if err != nil {
+		return nil, err
+	}
+	users := js["users"].([]map[string]interface{})
+	retval := make(ListUsersResult, len(users))
+	for i, v := range users {
+		retval[i] = ListUsersResultElement{
+			ID:            v["id"].(int),
+			FirstName:     v["first_name"].(string),
+			LastName:      v["last_name"].(string),
+			Email:         v["email"].(string),
+			Phone:         v["phone"].(string),
+			GravatarURL:   v["gravatar_url"].(string),
+			Administrator: v["administrator"].(bool),
+			CreatedAt:     v["created_at"].(time.Time),
+			UpdatedAt:     v["updated_at"].(time.Time),
+			Name:          v["name"].(string),
+			AccountOwner:  v["account_owner"].(bool),
+			Organization:  v["organization"].(string),
+			Roles:         v["roles"].([]string),
+		}
+	}
+	return &retval, nil
+}
---
+++ api_user_test.go
@@ -0,0 +1,91 @@
+//
+// Treasure Data API client for Go
+//
+// Copyright (C) 2014 Treasure Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package td_client
+
+import (
+	"testing"
+)
+
+func TestListUsers(t *testing.T) {
+	client, err := NewTDClient(Settings{
+		Transport: &DummyTransport{[]byte(listUsersResponse)},
+	})
+	if err != nil {
+		t.Fatalf("failed create client: %s", err.Error())
+	}
+	userList, err := client.ListUsers()
+	if err != nil {
+		t.Fatalf("bad request: %s", err.Error())
+	}
+	if len(*userList) != 3 {
+		t.Fatal("not expected user count")
+	}
+	t.Log(userList)
+}
+
+const listUsersResponse = `
+{
+    "users": [
+        {
+            "id": 1,
+            "first_name": "HOGE",
+            "last_name": "FUGA",
+            "email": "hogefuga@github.com",
+            "phone": "00-00-0000-0000",
+            "gravatar_url": "https://secure.gravatar.com/avatar/1c56a29a2f0c3114822fa47c4308410f?size=80",
+            "administrator": true,
+            "created_at": "2016-05-17T09:26:38Z",
+            "updated_at": "2017-06-07T22:09:34Z",
+            "name": "HOGE FUGA",
+            "account_owner": true,
+            "organization": null,
+            "roles": []
+        },
+        {
+            "id": 2,
+            "first_name": "FIZZ",
+            "last_name": "BUZZ",
+            "email": "fizzbuzz@github.com",
+            "phone": null,
+            "gravatar_url": "https://secure.gravatar.com/avatar/11b59b7feed4ee2995328a666ae2eb3f?size=80",
+            "administrator": false,
+            "created_at": "2016-05-30T09:20:18Z",
+            "updated_at": "2017-09-06T02:50:23Z",
+            "name": "FIZZ BUZZ",
+            "account_owner": false,
+            "organization": null,
+            "roles": []
+        },
+        {
+            "id": 3,
+            "first_name": null,
+            "last_name": null,
+            "email": "foovar@github.com",
+            "phone": null,
+            "gravatar_url": "https://secure.gravatar.com/avatar/304d83e6eb7fca0b44e3a3a4edb1e063?size=80",
+            "administrator": true,
+            "created_at": "2016-05-30T09:20:35Z",
+            "updated_at": "2016-07-13T09:42:32Z",
+            "name": "foovar@github.com",
+            "account_owner": false,
+            "organization": null,
+            "roles": []
+        }
+    ]
+}
+`
